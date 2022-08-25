c0232ddbaa93408eeb2472ab5156c4f101cc1e12
Walkr <davi.speck@walkr.app>
2022-08-25 15:52:55 -0300

    /

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'puta que pariu'})
+})
 app.get('/rapeize', (req, res) => {
     res.status(200).json({devJunior: ['Davi, Ale'], estag: 'Aenã'})
 })
